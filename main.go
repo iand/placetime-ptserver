7d89c80c0383f6d50b0433b4a6bba77cf22da2d1
Ian Davis <nospam@iandavis.com>
2013-05-20 17:17:22 +0100

    Small amount of code cleanup. Restricted adding of suggested profiles to admins

--- main.go
+++ main.go
@@ -564,54 +564,21 @@ func initData() {
 	applog.Infof("Resetting database")
 	s.ResetAll()
 
-	// s.AddProfile("ukfestivals", "sunshine", "UK Festivals", "Every musical festival in the UK.", "", "")
-	// s.AddItem("ukfestivals", "22 Jul 2012", "Isle of Wight Festival, Isle of Wight, Newport", "http://www.last.fm/festival/3162276+Isle+of+Wight+Festival")
-	// s.AddItem("ukfestivals", "24 Aug 2012", "Leeds Festival 2012, Bramham Park, Leeds", "http://www.last.fm/festival/2048182+Leeds+Festival+2012")
-	// s.AddItem("ukfestivals", "24 Aug 2012", "Reading Festival 2012, Little John's Farm, Reading", "http://www.last.fm/festival/2043239+Reading+Festival+2012")
-	// s.AddItem("ukfestivals", "27 Jul 2012", "Steelhouse Festival, Hafod-Y-Dafal Farm, Ebbw Vale", "http://www.last.fm/festival/3271130+Steelhouse+Festival")
-	// s.AddItem("ukfestivals", "17 Aug 2012", "Beautiful Days 2012, Escot Park, Nr Exeter Devon", "http://www.last.fm/festival/3218182+Beautiful+Days+2012")
-	// s.AddItem("ukfestivals", "27 Jul 2012", "Kendal Calling 2012, lowther deer park, Penrith, Cumbria", "http://www.last.fm/festival/3210406+Kendal+Calling+2012")
-	// s.AddItem("ukfestivals", "14 Jul 2012", "Summer Breeze Festival 2012, Liddington Warren Farm, Swindon", "http://www.last.fm/festival/3294945+Summer+Breeze+Festival+2012")
-	// s.AddItem("ukfestivals", "6 Sep 2012", "Bestival 2012, Robin Hill, Isle of Wight", "http://www.last.fm/festival/2067992+Bestival+2012")
-	// s.AddItem("ukfestivals", "24 Aug 2012", "Solfest 2012, Tarnside Farm, Tarns, Cumbria", "http://www.last.fm/festival/3236642+Solfest+2012")
-	// s.AddItem("ukfestivals", "19 Jul 2012", "The Secret Garden Party 2012, Abbots Ripton, Huntingdon", "http://www.last.fm/festival/3186405+The+Secret+Garden+Party+2012")
-	// s.AddItem("ukfestivals", "31 Aug 2012", "Merthyr Rock 2012, Cyfartha Park, Merthyr Tydfil", "http://www.last.fm/festival/3302416+Merthyr+Rock+2012")
-	// s.AddItem("ukfestivals", "1 Aug 2012", "Meltdown Festival 2012, Southbank Centre, London", "http://www.last.fm/festival/3284883+Meltdown+Festival+2012")
-	// s.AddItem("ukfestivals", "21 Aug 2012", "Tennent's Vital 2012, Boucher Playing Fields, Belfast", "http://www.last.fm/festival/3237701+Tennent%27s+Vital+2012")
-	// s.AddItem("ukfestivals", "3 Nov 2012", "Damnation Festival 2012, Leeds University Union, Leeds", "http://www.last.fm/festival/3242700+Damnation+Festival+2012")
-	// s.AddItem("ukfestivals", "3 Aug 2012", "Standon Calling, Standon Lordship, Standon", "http://www.last.fm/festival/3217298+Standon+Calling")
-	// s.AddItem("ukfestivals", "17 Aug 2012", "Summer Sundae 2012, De Montfort Hall, Leicester", "http://www.last.fm/festival/3210772+Summer+Sundae+2012")
-	// s.AddItem("ukfestivals", "24 Aug 2012", "Forest of Galtres Festival 2012, Crayke, York", "http://www.last.fm/festival/3314111+Forest+of+Galtres+Festival+2012")
-	// s.AddItem("ukfestivals", "3 Aug 2012", "Stockton Weekender, Riverside, Stockton-on-Tees", "http://www.last.fm/festival/3228328+Stockton+Weekender")
-	// s.AddItem("ukfestivals", "20 Jul 2012", "Wickerman, East Kirkcarswell, Dundrennan", "http://www.last.fm/festival/3317866+Wickerman")
-
-	// s.Delete("testdata")
-	// s.SetProfile(&Profile{Pid: "testdata", Name: "Test Data", Bio: "Loads of future events."})
-	// date := time.Now()
-	// for i := 0; i < 2000; i++ {
-	// 	date = date.Add(79 * time.Hour)
-	// 	s.AddItem("testdata", date.Format("02 Jan 2006"), fmt.Sprintf("Test data, item number %d", i), fmt.Sprintf("http://example.com/%d", i)))
-	// }
-
-	applog.Infof("Adding profile for iand")
+	applog.Infof("Adding profile for @iand")
 	err := s.AddProfile("@iand", "sunshine", "Ian", "Timefloes.", "", "", "nospam@iandavis.com", "", "", "", "")
 	if err != nil {
 		applog.Errorf("Could not add profile for @iand: %s", err.Error())
 	}
 
-	applog.Infof("Adding profile for daveg")
+	applog.Infof("Adding profile for @daveg")
 	err = s.AddProfile("@daveg", "sunshine", "Dave", "", "", "", "", "", "", "", "")
 	if err != nil {
 		applog.Errorf("Could not add profile for @daveg: %s", err.Error())
 	}
 	s.AddSuggestedProfile("iand", "london")
 
-	//s.Follow("iand", "nasa")
-
 	applog.Infof("Adding profile for nasa")
 	s.AddProfile("@nasa", "nasa", "Nasa Missions", "Upcoming NASA mission information.", "", "", "", "", "", "", "")
-
-	// s.Follow("@iand", "@nasa")
 
 	applog.Infof("Adding items for nasa")
 	s.AddItem("@nasa", parseKnownTime("1 Jan 2015"), "BepiColombo - Launch of ESA and ISAS Orbiter and Lander Missions to Mercury", "", "", "")
@@ -625,24 +592,6 @@ func initData() {
 	s.AddItem("@nasa", parseKnownTime("1 May 2014"), "Rosetta - ESA mission reaches Comet Churyumov-Gerasimenko", "", "", "")
 	s.AddItem("@nasa", parseKnownTime("1 Jan 2014"), "Mars Sample Return Mission - Launch of NASA sample return mission to Mars", "", "", "")
 	s.AddItem("@nasa", parseKnownTime("5 Apr 2231"), "Pluto - is passed by Neptune in distance from the Sun for the next 20 years", "", "", "")
-
-	//http: //www.ents24.com/web/festival-tickets/T-In-The-Park-2013-2998409.html
-
-	// s.SetProfile(&Profile{Pid: "o2shepherdsbushempire ", Name: "O2 Shepherd's Bush Empire Events", Bio: "", Feed: "http://www.o2shepherdsbushempire.co.uk/RSS"})
-	// s.SetProfile(&Profile{Pid: "skiddlewc2", Name: "Skiddle WC2", Bio: "What's On in London and area", Feed: "http://www.skiddle.com/rss/events.php?c=WC2"})
-
-	// s.AddProfile("artscouncil", "sunshine", "Action Space London Events Ltd | Arts Council", "", "http://www.artscouncil.org.uk/feeds/latest/", "")
-	// s.AddProfile("soas", "sunshine", "Calendar of Events - School of Oriental and African Studies", "", "http://www.soas.ac.uk/about/events/rss-events.php?type=month&amp;format=rss2.0&amp;filter=general", "")
-	// s.AddProfile("lms", "sunshine", "Calendar of Events | London Mathematical Society", "", "http://www.lms.ac.uk/rss.xml", "")
-	// s.AddProfile("designmuseum", "sunshine", "Design Museum - Evening Talks and Design Lectures", "", "http://designmuseum.org/feed", "")
-	// s.AddProfile("forbiddenplanet", "sunshine", "Events @ Forbidden Planet", "", "http://feeds.forbiddenplanet.com/fplatestevents", "")
-	// s.AddProfile("bbk", "sunshine", "Events calendar — Birkbeck, University of London", "", "http://www.bbk.ac.uk/news/aggregator/RSS", "")
-	// s.AddProfile("gold", "sunshine", "Events Calendar, Goldsmiths, University of London", "", "http://www.gold.ac.uk/apps/events/rss.php", "")
-	// s.AddProfile("arts", "sunshine", "Events Listings - University of the Arts London", "", "http://newsevents.arts.ac.uk/events/feed/arts/", "")
-	// s.AddProfile("fineartslondon", "sunshine", "Fine Art London Events Calendar", "", "http://www.fineartslondon.com/feed/", "")
-	// s.AddProfile("flavorpill", "sunshine", "Flavorpill London Home", "", "http://feeds.feedburner.com/flavorpill/ldn", "")
-	// s.AddProfile("wellcomecollection", "sunshine", "Free events in London - Wellcome Collection", "", "http://www.wellcomecollection.org/feeds/events.aspx", "")
-	// s.AddProfile("indymedia", "sunshine", "Indymedia London | Events | Index", "", "http://london.indymedia.org/events.rss", "")
 
 	applog.Infof("Adding profile for @visitlondon")
 	err = s.AddProfile("@visitlondon", "sunshine", "visitlondon.com", "", "", "", "", "", "", "", "")
@@ -673,25 +622,6 @@ func initData() {
 	if err != nil {
 		applog.Errorf("Could not add profile for o2shepherdsbushempire: %s", err.Error())
 	}
-	// s.AddProfile("naturelondonscience", "sunshine", "London Blog: Science Events In London This Week : London Blog", "", "http://blogs.nature.com/london/feed", "")
-	// s.AddProfile("lcf", "sunshine", "London College of Fashion - News &amp; Events", "", "http://newsevents.arts.ac.uk/lcf/news/feed/arts/", "")
-	// s.AddProfile("frenchcinemalondon", "sunshine", "London events | French Cinema London", "", "http://www.frenchcinemalondon.com/?feed=rss2", "")
-	// s.AddProfile("londonfashionweek", "sunshine", "London Fashion Week : Events Schedule", "", "http://www.londonfashionweek.com/news_xml.aspx", "")
-	// s.AddProfile("londonhistorians", "sunshine", "London Historians: Events", "", "http://londonhistorians.wordpress.com/feed/", "")
-	// s.AddProfile("londonnightclubs", "sunshine", "London Nightclubs | London Clubbing Guide | Clubs in London", "", "http://skiddle.com/rss/events.php?c=WC2", "")
-	// s.AddProfile("londonwarehouseevents", "sunshine", "London Warehouse Events", "", "http://londonwarehouseevents.co.uk/feed/atom/", "")
-	// s.AddProfile("npg", "sunshine", "National Portrait Gallery | Face to Face blog: Events", "", "http://face2face.si.edu/my_weblog/atom.xml", "")
-	// s.AddProfile("o2shepherdsbushempire", "sunshine", "O2 Shepherd's Bush Empire | Concert Dates and Tickets", "", "http://www.o2shepherdsbushempire.co.uk/RSS", "")
-	// s.AddProfile("thebrickhouse", "sunshine", "The Brickhouse | Restaurant Bar, Cabaret Club, Private &amp; Corporate ...", "", "http://www.thebrickhouse.co.uk/feed/", "")
-	// s.AddProfile("nhm", "sunshine", "The Natural History Museum, London events | Ecsite", "", "http://www.ecsite.eu/news_and_events/470/events.rss", "")
-	// s.AddProfile("tntlondon", "sunshine", "TNT Magazine Events in London, things to do in London, London ...", "", "http://www.tntmagazine.com/london/events/rss", "")
-	// s.AddProfile("royalarmouries", "sunshine", "Tower of London Events | Royal Armouries", "", "http://www.royalarmouries.org/rss/events", "")
-	// s.AddProfile("uwl", "sunshine", "University of West London: events", "", "http://www.uwl.ac.uk/the_university/news/TVU_news_rss.xml", "")
-	// s.AddProfile("urbanjunkieslondon", "sunshine", "Urban Junkies London", "", "http://www.urbanjunkies.com/london/feed/rss.html", "")
-	// s.AddProfile("royalacademy", "sunshine", "Van Gogh's London - Events &amp; resources - The Real Van Gogh: The ...", "", "http://www.royalacademy.org.uk/events.xml", "")
-	// s.AddProfile("thevintageguidetolondon", "sunshine", "vintage events London | The Vintage Guide to LondonThe Vintage ...", "", "http://www.thevintageguidetolondon.com/feed/atom/", "")
-	// s.AddProfile("jewishmuseum", "sunshine", "What's on - The Jewish Museum London", "", "http://www.jewishmuseum.org.uk/rss", "")
-	// s.AddProfile("architecture", "sunshine", "What's on? - Royal Institute of British Architects", "", "http://www.architecture.com/syndication.riba?feed_type=Events", "")
 
 	applog.Infof("Adding follows for @iand")
 	s.Follow("@iand", "londonsportsguide")
@@ -719,11 +649,6 @@ func vocabRedirectHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func adminHandler(w http.ResponseWriter, r *http.Request) {
-	// sessionValid, sessionPid := checkSession(w, r, false)
-	// if !sessionValid {
-	// 	return
-	// }
-	// TODO: restrict to admins
 	sessionValid, sessionPid := checkSession(w, r, false)
 	if !sessionValid {
 		return
@@ -845,12 +770,15 @@ func demoteHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func addSuggestHandler(w http.ResponseWriter, r *http.Request) {
-	sessionValid, _ := checkSession(w, r, false)
+
+	sessionValid, sessionPid := checkSession(w, r, false)
 	if !sessionValid {
 		return
 	}
-	// TODO: restrict to admins
-
+	if !isAdmin(sessionPid) {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
 	pid := r.FormValue("pid")
 	loc := r.FormValue("loc")
 	s := datastore.NewRedisStore()
@@ -865,12 +793,14 @@ func addSuggestHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func remSuggestHandler(w http.ResponseWriter, r *http.Request) {
-	sessionValid, _ := checkSession(w, r, false)
+	sessionValid, sessionPid := checkSession(w, r, false)
 	if !sessionValid {
 		return
 	}
-	// TODO: restrict to admins
-
+	if !isAdmin(sessionPid) {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
 	pid := r.FormValue("pid")
 	loc := r.FormValue("loc")
 	s := datastore.NewRedisStore()
@@ -1075,15 +1005,7 @@ func updateProfileHandler(w http.ResponseWriter, r *http.Request) {
 		if _, exists := r.Form[p]; exists {
 			values[p] = r.FormValue(p)
 		}
-
 	}
-
-	// name := r.FormValue("pname")
-	// feedurl := r.FormValue("feedurl")
-	// bio := r.FormValue("bio")
-	// // email := r.FormValue("email")
-	// parentpid := r.FormValue("parentpid")
-
 	s := datastore.NewRedisStore()
 	defer s.Close()
 
@@ -1142,35 +1064,18 @@ func flagProfileHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func twitterHandler(w http.ResponseWriter, r *http.Request) {
-
-	// callback := fmt.Sprintf("http://%s/-soauth", Hostname())
-
-	// token, tokenSecret, callbackConfirmed, err := GetTwitterRequestToken(callback)
-	// _ = tokenSecret
-	// _ = callbackConfirmed
-	// applog.Debugf("token: %s", token)
-	// applog.Debugf("tokenSecret: %s", tokenSecret)
-	// applog.Debugf("callbackConfirmed: %s", callbackConfirmed)
-
-	// if err != nil {
-	// 	ErrorResponse(w, r, err)
-	// 	return
-	// }
-
 	service := OauthService()
 
 	httpClient := new(http.Client)
 	userConfig := &oauth1a.UserConfig{}
 	err := userConfig.GetRequestToken(service, httpClient)
 	if err != nil {
-		applog.Errorf("Problem getting the request token: %v", err)
-		ErrorResponse(w, r, errors.New("Problem getting the request token"))
+		ErrorResponse(w, r, err)
 		return
 	}
 	url, err := userConfig.GetAuthorizeURL(service)
 	if err != nil {
-		applog.Errorf("Problem getting the authorization URL: %v", err)
-		ErrorResponse(w, r, errors.New("Problem getting the authorization URL"))
+		ErrorResponse(w, r, err)
 		return
 	}
 
@@ -1204,9 +1109,6 @@ func soauthHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	applog.Infof("Access Token: %s", userConfig.AccessTokenKey)
-	applog.Infof("Token Secret: %s", userConfig.AccessTokenSecret)
-	applog.Infof("Screen name: %s", userConfig.AccessValues.Get("screen_name"))
 	screenName := userConfig.AccessValues.Get("screen_name")
 
 	client := twittergo.NewClient(service.ClientConfig, userConfig)
@@ -1225,6 +1127,7 @@ func soauthHandler(w http.ResponseWriter, r *http.Request) {
 	contents, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		applog.Errorf("Received an error while reading twitter response: %s", err)
+		ErrorResponse(w, r, err)
 		return
 	}
 
@@ -1234,15 +1137,6 @@ func soauthHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		ErrorResponse(w, r, err)
 	}
-
-	//	println(string(contents))
-
-	applog.Infof("Twitter Name: %s", twitterUserData.Name)
-	applog.Infof("Twitter Location: %s", twitterUserData.Location)
-	applog.Infof("Twitter Description: %s", twitterUserData.Description)
-	applog.Infof("Twitter Url: %s", twitterUserData.Url)
-	applog.Infof("Twitter ProfileImageUrl: %s", twitterUserData.ProfileImageUrl)
-	applog.Infof("Twitter ProfileImageUrlHttps: %s", twitterUserData.ProfileImageUrlHttps)
 
 	pid := fmt.Sprintf("@%s", screenName)
 
