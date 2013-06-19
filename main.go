10afda195c8da0163281f487aca4a3c3331bc998
Ian Davis <nospam@iandavis.com>
2013-06-19 14:11:43 +0100

    Tightened up media types on items and added times

--- main.go
+++ main.go
@@ -565,60 +565,60 @@ func initData() {
 	s.ResetAll()
 
 	applog.Infof("Adding profile for @iand")
-	err := s.AddProfile("@iand", "sunshine", "Ian", "Timefloes.", "", "", "nospam@iandavis.com", "", "", "", "")
+	err := s.AddProfile("@iand", "sunshine", "Ian", "Timefloes.", "", "", "nospam@iandavis.com", "", "", "", "", "")
 	if err != nil {
 		applog.Errorf("Could not add profile for @iand: %s", err.Error())
 	}
 
 	applog.Infof("Adding profile for @daveg")
-	err = s.AddProfile("@daveg", "sunshine", "Dave", "", "", "", "", "", "", "", "")
+	err = s.AddProfile("@daveg", "sunshine", "Dave", "", "", "", "", "", "", "", "", "")
 	if err != nil {
 		applog.Errorf("Could not add profile for @daveg: %s", err.Error())
 	}
 	s.AddSuggestedProfile("@iand", "london")
 
 	applog.Infof("Adding profile for @nasa")
-	s.AddProfile("@nasa", "nasa", "Nasa Missions", "Upcoming NASA mission information.", "", "", "", "", "", "", "")
+	s.AddProfile("@nasa", "nasa", "Nasa Missions", "Upcoming NASA mission information.", "", "", "", "", "", "", "", "")
 
 	applog.Infof("Adding items for nasa")
-	s.AddItem("@nasa", parseKnownTime("1 Jan 2015"), "BepiColombo - Launch of ESA and ISAS Orbiter and Lander Missions to Mercury", "", "", "")
-	s.AddItem("@nasa", parseKnownTime("26 Aug 2012"), "Dawn - Leaves asteroid Vesta, heads for asteroid 1 Ceres", "", "", "")
-	s.AddItem("@nasa", parseKnownTime("1 Sep 2012"), "BepiColombo - Launch of ESA and ISAS Orbiter and Lander Missions to Mercury", "", "", "")
-	s.AddItem("@nasa", parseKnownTime("1 Feb 2015"), "Dawn - Goes into orbit around asteroid 1 Ceres", "", "", "")
-	s.AddItem("@nasa", parseKnownTime("14 Jul 2015"), "New Horizons - NASA mission reaches Pluto and Charon", "", "", "")
-	s.AddItem("@nasa", parseKnownTime("1 Mar 2013"), "LADEE - Launch of NASA Orbiter to the Moon", "", "", "")
-	s.AddItem("@nasa", parseKnownTime("1 Nov 2014"), "Philae - ESA Rosetta Lander touches down on Comet Churyumov-Gerasimenko", "", "", "")
-	s.AddItem("@nasa", parseKnownTime("1 Nov 2013"), "MAVEN - Launch of Mars Orbiter", "", "", "")
-	s.AddItem("@nasa", parseKnownTime("1 May 2014"), "Rosetta - ESA mission reaches Comet Churyumov-Gerasimenko", "", "", "")
-	s.AddItem("@nasa", parseKnownTime("1 Jan 2014"), "Mars Sample Return Mission - Launch of NASA sample return mission to Mars", "", "", "")
-	s.AddItem("@nasa", parseKnownTime("5 Apr 2231"), "Pluto - is passed by Neptune in distance from the Sun for the next 20 years", "", "", "")
+	s.AddItem("@nasa", parseKnownTime("1 Jan 2015"), "BepiColombo - Launch of ESA and ISAS Orbiter and Lander Missions to Mercury", "", "", "", "")
+	s.AddItem("@nasa", parseKnownTime("26 Aug 2012"), "Dawn - Leaves asteroid Vesta, heads for asteroid 1 Ceres", "", "", "", "")
+	s.AddItem("@nasa", parseKnownTime("1 Sep 2012"), "BepiColombo - Launch of ESA and ISAS Orbiter and Lander Missions to Mercury", "", "", "", "")
+	s.AddItem("@nasa", parseKnownTime("1 Feb 2015"), "Dawn - Goes into orbit around asteroid 1 Ceres", "", "", "", "")
+	s.AddItem("@nasa", parseKnownTime("14 Jul 2015"), "New Horizons - NASA mission reaches Pluto and Charon", "", "", "", "")
+	s.AddItem("@nasa", parseKnownTime("1 Mar 2013"), "LADEE - Launch of NASA Orbiter to the Moon", "", "", "", "")
+	s.AddItem("@nasa", parseKnownTime("1 Nov 2014"), "Philae - ESA Rosetta Lander touches down on Comet Churyumov-Gerasimenko", "", "", "", "")
+	s.AddItem("@nasa", parseKnownTime("1 Nov 2013"), "MAVEN - Launch of Mars Orbiter", "", "", "", "")
+	s.AddItem("@nasa", parseKnownTime("1 May 2014"), "Rosetta - ESA mission reaches Comet Churyumov-Gerasimenko", "", "", "", "")
+	s.AddItem("@nasa", parseKnownTime("1 Jan 2014"), "Mars Sample Return Mission - Launch of NASA sample return mission to Mars", "", "", "", "")
+	s.AddItem("@nasa", parseKnownTime("5 Apr 2231"), "Pluto - is passed by Neptune in distance from the Sun for the next 20 years", "", "", "", "")
 
 	applog.Infof("Adding profile for @visitlondon")
-	err = s.AddProfile("@visitlondon", "sunshine", "visitlondon.com", "", "", "", "", "", "", "", "")
+	err = s.AddProfile("@visitlondon", "sunshine", "visitlondon.com", "", "", "", "", "", "", "", "", "")
 	if err != nil {
 		applog.Errorf("Could not add profile for @visitlondon: %s", err.Error())
 	}
 
 	applog.Infof("Adding feed profile for londonsportsguide")
-	err = s.AddProfile("londonsportsguide", "sunshine", "Football in London - visitlondon.com", "", "http://feeds.visitlondon.com/LondonSportsGuide", "@visitlondon", "", "", "", "", "")
+	err = s.AddProfile("londonsportsguide", "sunshine", "Football in London - visitlondon.com", "", "http://feeds.visitlondon.com/LondonSportsGuide", "@visitlondon", "", "", "", "", "", "event")
 	if err != nil {
 		applog.Errorf("Could not add profile for londonsportsguide: %s", err.Error())
 	}
 
 	applog.Infof("Adding feed profile for londonartsguide")
-	err = s.AddProfile("londonartsguide", "sunshine", "London Arts Guide - visitlondon.com", "", "http://feeds.visitlondon.com/LondonArtsGuide", "@visitlondon", "", "", "", "", "")
+	err = s.AddProfile("londonartsguide", "sunshine", "London Arts Guide - visitlondon.com", "", "http://feeds.visitlondon.com/LondonArtsGuide", "@visitlondon", "", "", "", "", "", "event")
 	if err != nil {
 		applog.Errorf("Could not add profile for londonartsguide: %s", err.Error())
 	}
 
 	applog.Infof("Adding feed profile for londondanceguide")
-	err = s.AddProfile("londondanceguide", "sunshine", "London Dance Guide - visitlondon.com", "", "http://feeds.visitlondon.com/LondonDanceGuide", "@visitlondon", "", "", "", "", "")
+	err = s.AddProfile("londondanceguide", "sunshine", "London Dance Guide - visitlondon.com", "", "http://feeds.visitlondon.com/LondonDanceGuide", "@visitlondon", "", "", "", "", "", "event")
 	if err != nil {
 		applog.Errorf("Could not add profile for londondanceguide: %s", err.Error())
 	}
 
 	applog.Infof("Adding feed profile for o2shepherdsbushempire")
-	err = s.AddProfile("o2shepherdsbushempire", "sunshine", "O2 Shepherd's Bush Empire | Concert Dates and Tickets", "", "http://www.o2shepherdsbushempire.co.uk/RSS", "", "", "", "", "", "")
+	err = s.AddProfile("o2shepherdsbushempire", "sunshine", "O2 Shepherd's Bush Empire | Concert Dates and Tickets", "", "http://www.o2shepherdsbushempire.co.uk/RSS", "", "", "", "", "", "", "event")
 	if err != nil {
 		applog.Errorf("Could not add profile for o2shepherdsbushempire: %s", err.Error())
 	}
@@ -679,8 +679,9 @@ func addHandler(w http.ResponseWriter, r *http.Request) {
 	link := r.FormValue("link")
 	ets := r.FormValue("ets")
 	image := r.FormValue("image")
+	media := r.FormValue("media")
 
-	applog.Debugf("Adding item pid: %s, text: %s, link: %s, ets: %v, image: %s", pid, text, link, ets, image)
+	applog.Debugf("Adding item pid: %s, text: %s, link: %s, ets: %v, image: %s, media: %s", pid, text, link, ets, image, media)
 
 	s := datastore.NewRedisStore()
 	defer s.Close()
@@ -693,7 +694,7 @@ func addHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	itemid, err := s.AddItem(pid, etsParsed, text, link, image, "")
+	itemid, err := s.AddItem(pid, etsParsed, text, link, image, "", media)
 	if err != nil {
 		ErrorResponse(w, r, err)
 		return
@@ -958,6 +959,7 @@ func addProfileHandler(w http.ResponseWriter, r *http.Request) {
 	bio := r.FormValue("bio")
 	parentpid := datastore.PidType(r.FormValue("parentpid"))
 	email := r.FormValue("email")
+	itemType := r.FormValue("itemtype")
 
 	var err error
 	if pwd == "" {
@@ -972,7 +974,7 @@ func addProfileHandler(w http.ResponseWriter, r *http.Request) {
 	s := datastore.NewRedisStore()
 	defer s.Close()
 
-	err = s.AddProfile(pid, pwd, name, bio, feedurl, parentpid, email, "", "", "", "")
+	err = s.AddProfile(pid, pwd, name, bio, feedurl, parentpid, email, "", "", "", "", itemType)
 	if err != nil {
 		ErrorResponse(w, r, err)
 		return
@@ -1156,7 +1158,7 @@ func soauthHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		err = s.AddProfile(pid, pwd, twitterUserData.Name, twitterUserData.Description, "", "", "", twitterUserData.Location, twitterUserData.Url, twitterUserData.ProfileImageUrl, twitterUserData.ProfileImageUrlHttps)
+		err = s.AddProfile(pid, pwd, twitterUserData.Name, twitterUserData.Description, "", "", "", twitterUserData.Location, twitterUserData.Url, twitterUserData.ProfileImageUrl, twitterUserData.ProfileImageUrlHttps, "")
 		if err != nil {
 			ErrorResponse(w, r, err)
 			return
